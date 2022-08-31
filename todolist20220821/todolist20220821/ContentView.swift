42c13bb268803f57062fef9201f2172dd856c77a
a-im12 <ayumu.git@gmail.com>
2022-08-31 16:30:34 +0900

    完成👏

---
+++ todolist20220821/todolist20220821/ContentView.swift
@@ -0,0 +1,104 @@
+//
+//  ContentView.swift
+//  todolist20220821
+//
+//  Created by いーま on 2022/08/21.
+//
+
+import SwiftUI
+
+struct TaskData{
+    var title:String
+    var completed:Bool
+    var id = UUID()
+}
+
+struct ContentView: View {
+    
+    init(){
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor.clear
+    }
+    
+    @State var appendTask:Bool = false
+    @State var taskList:Array<TaskData> = []
+    @State var task:String = ""
+    @State var rowCnt:Int = 0
+    
+    var body: some View {
+        if self.appendTask {
+            NavigationView{
+                List{
+                    TextField("タスクを入力してください", text: $task)
+                    
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            print("確定")
+                            if task != ""{
+                                taskList.append(TaskData(title: task, completed: false))
+                                task = ""
+                                appendTask.toggle()
+                            }
+                        }){
+                            Text("確定")
+                        }.foregroundColor(Color.blue)
+                        Spacer()
+                        
+                    }
+                    
+                }.navigationTitle("追加")
+                    .navigationBarItems(leading: Button(action: {
+//                        print("戻る")
+                        appendTask.toggle()
+                    }){
+                        Image(systemName: "lessthan")
+                    })
+            }
+        }else{
+            NavigationView{
+                VStack{
+                    List(0..<taskList.count, id:\.self){ index in
+                        Button(action:{
+//                            print("ボタンが押されたよ")
+                            
+                            taskList[index].completed.toggle()
+                        }){
+                            HStack{
+                                Image(systemName:taskList[index].completed ? "checkmark.circle.fill" : "circle")
+                                Text(taskList[index].title)
+                            }
+                        }.foregroundColor(.black)
+                    }.navigationTitle("ToDoリスト")
+                        .navigationBarItems(leading: Button(action: {
+                            self.rowCnt = taskList.count
+                            for i in 0..<taskList.count{
+                                print((self.rowCnt - 1) - i)
+                                if taskList[(self.rowCnt - 1) - i].completed{
+                                    taskList.remove(at: (self.rowCnt - 1) - i)
+                                }
+                                
+                            }
+                        }){
+                            Text("削除")
+                        }, trailing: Button(action: {
+                            appendTask.toggle()
+                        }){
+                            Text("追加")
+                        })
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+            
+    }
+}
---
+++ todolist20220821/todolist20220821/todolist20220821App.swift
@@ -0,0 +1,17 @@
+//
+//  todolist20220821App.swift
+//  todolist20220821
+//
+//  Created by いーま on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct todolist20220821App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
